d86fc5182932c2c1aab59affa4542cd8ea97f18d
el mehdi <bakhti.elmehdi@gmail.com>
2023-07-11 15:13:00 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,46 +1,35 @@
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * *string_nconcat - the function that concatenates n bytes of a string to another string.
- * @s1: the string to append to.
- * @s2: the string to concatenate from.
- * @n: number of bytes from s2 to concatenate to s1.
+ * *_strdup - the function that duplicates a string.
  *
- * Return: pointer to the resulting string.
- */
+ * @str: input string to duplicate.
+ *
+ * Return: NULL if str == NULL
+ *         @str
+*/
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *_strdup(char *str)
 {
+	int i = 0, l = 0;
 	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (!s)
+	if (str == NULL)
 		return (NULL);
 
-	while (i < len1)
+	/*calculate size of str*/
+	while (str[l] != '\0')
+		l++;
+
+	s = malloc((l + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
 	{
-		s[i] = s1[i];
+		s[i] = str[i];
 		i++;
 	}
-
-	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
-
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
-
-	s[i] = '\0';
 
 	return (s);
 }
