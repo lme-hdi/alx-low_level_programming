7c2ba1268fa4c0e724230041a7ebf8e15b3eca03
el mehdi <bakhti.elmehdi@gmail.com>
2023-07-22 14:14:40 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the function that prints the name of the file.
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
